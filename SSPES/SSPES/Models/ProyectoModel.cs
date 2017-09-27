944e8eb87e28beb249d066af99a1fc12028229b4
wilmercastrillon <wilmercastrillon98@gmail.com>
2017-09-27 18:03:55 -0500

    ajustes variables
    ajustes

---
+++ SSPES/SSPES/Models/ProyectoModel.cs
@@ -0,0 +1,33 @@
+﻿using SSPES.BD;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace SSPES.Models {
+    public class ProyectoModel {
+
+        private Conexion con = new Conexion();
+        protected string nombre { get; set; }
+        protected string descripcion { get; set; }
+
+        public ProyectoModel(string a, string b) {
+            nombre = a;
+            descripcion = b;
+        }
+
+        public ProyectoModel() {
+        }
+
+        public List<string> ConsultarProyectos() {
+            List<string> lista = new List<string>();
+            string sql = "SELECT NOMBRE FROM proyecto WHERE ESTADO = 'A';";
+            DataTable data = con.EjecutarConsulta(sql, CommandType.Text);
+            foreach (DataRow row in data.Rows) {
+                lista.Add(row[0].ToString());
+            }
+            return lista;
+        }
+    }
+}
